e2f1088af897b7d134fa5d8e15ba067ee5995c01
mushfiq-rahman <mushfiq.style@gmail.com>
2022-09-04 09:11:19 +0600

    final updated

--- frontend/components/data/skillData.js
+++ frontend/components/data/skillData.js
@@ -3,37 +3,37 @@ export const data=[
         id: 1,
         name: 'Data Science & Machine Learning',
         image: '/assets/course/dataSch.webp',
-        liveUrl: "/DataScience",
+        liveUrl: "/dataScience",
     },
     {
         id: 2,
         name: 'Graphics Design',
         image: '/assets/course/graphicsDesign.png',
-        liveUrl: "/",
+        liveUrl: "/graphicsDesign",
     },
     {
         id: 3,
         name: 'Digital Marketing',
         image: '/assets/course/digitalMarketing.webp',
-        liveUrl: "/",
+        liveUrl: "/digitalMarketing",
     },
     {
         id: 4,
         name: 'Web Design & Development',
         image: '/assets/course/webDevelopment.png',
-        liveUrl: "/",
+        liveUrl: "/webDevelopment",
     },
     {
         id: 5,
         name: 'Call Center Support',
         image: '/assets/course/callCenter.png',
-        liveUrl: "/",
+        liveUrl: "/callCenterSupport",
     },
 
     {
         id: 6,
         name: 'Robotics',
         image: '/assets/course/robotics.png',
-        liveUrl: "/",
+        liveUrl: "/robotics",
     },
     ]
